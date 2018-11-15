78d2018be99793b380aad443f8dd00402b23de91
xrl6238 <3jR0$am8>
2018-11-15 19:10:00 +0800

    fixbug

--- common/src/main/java/com/itmuch/cloud/study/common/Application.java
+++ common/src/main/java/com/itmuch/cloud/study/common/Application.java
@@ -1,11 +1,8 @@
 package com.itmuch.cloud.study.common;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 
 @SpringBootApplication
 public class Application {
